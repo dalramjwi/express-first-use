c82ce746878dc15a504086bbfced5ae409382898
dalramjwi <dalramjwi@gmail.com>
2024-06-12 17:31:05 +0900

    html finish

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 app.get("/", function (req, res) {
-  res.sendFile("/index.html");
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 app.use((req, res) => {
   res.type("text/plain");

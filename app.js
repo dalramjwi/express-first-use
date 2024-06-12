391428dbe3ddb62c252b9b40eaa8ee3d4f9cef13
dalramjwi <dalramjwi@gmail.com>
2024-06-12 17:30:15 +0900

    try to use sendFile

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 app.get("/", function (req, res) {
-  res.send(index.html);
+  res.sendFile("/index.html");
 });
 app.use((req, res) => {
   res.type("text/plain");

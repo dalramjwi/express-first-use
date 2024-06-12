c9cff37eda9b156ca73ba9cd73ceef65aa381834
dalramjwi <dalramjwi@gmail.com>
2024-06-12 17:29:12 +0900

    try use html

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  res.send(index.html);
 });
-// app.use((req, res) => {
-//   res.type("text/plain");
-//   res.status(404);
-//   res.send("404 - Not Found");
-// });
+app.use((req, res) => {
+  res.type("text/plain");
+  res.status(404);
+  res.send("404 - Not Found");
+});
 app.listen(port, () => console.log(`server is running ${port}`));
